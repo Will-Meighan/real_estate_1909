6a712df8f09389478152ccc73c745b7d627dc0f0
William Meighan <williamdmeighan@gmail.com>
2019-10-09 10:52:51 -0600

    Lots of changes, sorry I can't be more specific

--- lib/house.rb
+++ lib/house.rb
@@ -1,16 +1,34 @@
 class House
 
-  attr_reader :price, :address
+  attr_reader :price, :address, :length, :width
   attr_accessor :rooms
 
   def initialize(price, address)
     @price = price
     @address = address
     @rooms = []
+    @length = length
+    @width = width
   end
 
   def add_room(new_room)
     @rooms << new_room
+  end
+
+  def rooms_from_category(category)
+    rooms_from_category = []
+    rooms.each do |room|
+      if room.category == category
+        rooms_from_category << room
+      end
+    end
+    rooms_from_category
+  end
+
+  def area
+    #length times width for each room, then add rooms
+    room_area = @room_1(length * width)
+
   end
 
 end
--- test/house_test.rb
+++ test/house_test.rb
@@ -9,6 +9,8 @@ class HouseTest < Minitest::Test
     @house = House.new("$400000", "123 sugar lane")
     @room_1 = Room.new(:bedroom, 10, 13)
     @room_2 = Room.new(:bedroom, 11, 15)
+    @room_3 = Room.new(:living_room, 25, 15)
+    @room_4 = Room.new(:basement, 30, 41)
   end
 
   def test_it_exists
@@ -36,11 +38,23 @@ class HouseTest < Minitest::Test
   end
 
   def test_room_1_can_be_added_to_house
+    skip
     assert_equal @rooms, @house.add_room(@room_1)
   end
 
   def test_room_2_can_be_added_to_house
+    skip
     assert_equal @rooms, @house.add_room(@room_2)
   end
+
+  def test_it_can_calculate_number_of_rooms_in_category
+    skip
+    assert_equal [@room_1, @room_2], @house.rooms_from_category(:bedroom)
+  end
+
+  def test_it_can_calculate_total_area_of_all_rooms_in_house
+    assert_equal 1900, @house.area
+  end
+
 
 end
